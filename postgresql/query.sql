6d4eb27716ed43d0c8b7393ee2a3b2c3eeefd10a
ppolydoras <pavlos@raw-labs.com>
2024-11-28 12:30:23 +0200

    Improved documentation

--- postgresql/query.sql
+++ postgresql/query.sql
@@ -1,7 +1,7 @@
 -- This is a placeholder SQL query.
 -- You can replace this query with any valid SQL query against your PostgreSQL database.
 -- For example:
--- SELECT * FROM your_database.your_table;
+-- SELECT * FROM postgresql.<your_table>;
 -- If you introduce parameters, be sure to define them using RAW's parameter syntax.
 
 SELECT 'Hello, World!' AS greeting;
