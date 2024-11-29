a4991a0dd0b7d7a5cc38f097d24a89966eb4bb62
ppolydoras <pavlos@raw-labs.com>
2024-11-29 18:36:27 +0200

    added docs link to query params

--- postgresql/query.sql
+++ postgresql/query.sql
@@ -3,5 +3,6 @@
 -- For example:
 -- SELECT * FROM postgresql.<your_table>;
 -- If you introduce parameters, be sure to define them using RAW's parameter syntax.
+-- For further information, please check the online docs: https://docs.raw-labs.com/sql/examples/#modifying-the-example-to-add-a-query-parameter
 
 SELECT 'Hello, World!' AS greeting;
